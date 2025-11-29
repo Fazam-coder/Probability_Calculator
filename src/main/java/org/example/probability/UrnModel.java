1b4633975f9a70966b260a5b83270762fd249e38
Fazam-coder <amirrf06@gmail.com>
2025-11-29 14:49:08 +0300

    add urn model

---
+++ src/main/java/org/example/probability/UrnModel.java
@@ -0,0 +1,12 @@
+package org.example.probability;
+
+import static org.example.combinatorics.CombNoRepetitions.getCombinationsCount;
+
+public class UrnModel {
+    public static double getProbabilityAllMarked(int n, int m, int k) {
+        return ((double) getCombinationsCount(m, k)) / getCombinationsCount(n, k);
+    }
+    public static double getProbabilityRMarked(int n, int m, int k, int r) {
+        return ((double) getCombinationsCount(m, r) * getCombinationsCount(n - m, k - r)) / getCombinationsCount(n, k);
+    }
+}
